755ce3d01be66a9f470a548d8ea8fb01152d1324
fbngrm <fbngrm@gmail.com>
2019-12-30 13:02:19 +0100

    fix test for update

--- store/player/store_test.go
+++ store/player/store_test.go
@@ -46,34 +46,55 @@ func TestInsert(t *testing.T) {
 	}
 }
 
-// func TestUpdate(t *testing.T) {
-// 	db, mock, err := sqlmock.New()
-// 	if err != nil {
-// 		t.Fatalf("unexpected error %v", err)
-// 	}
-// 	defer db.Close()
+// changes the roster id and status
+func TestUpdate(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("unexpected error %v", err)
+	}
+	defer db.Close()
 
-// 	query := `UPDATE players SET roster_id = \$1 WHERE id = \$2`
-// 	mock.ExpectExec(query).WillReturnResult(sqlmock.NewResult(1, 1))
+	// before we actually execute our api function, we need to expect required DB actions
+	rows := sqlmock.NewRows([]string{"id", "roster_id", "first_name", "last_name", "alias", "active"}).
+		AddRow(182919996442279937, 1, "Dominic", "Luklowski", "DataSlayer9", "benched")
 
-// 	p := store.Player{
-// 		PlayerID:  182919996442279937,
-// 		RosterID:  382574876546039808,
-// 		FirstName: "Dominic",
-// 		LastName:  "Luklowski",
-// 		Alias:     "DataSlayer9",
-// 		Status:    "active",
-// 	}
-// 	ps := New(database.New(db, "mock-db", 0))
-// 	got, err := ps.Update(context.Background(), p)
-// 	if err != nil {
-// 		t.Fatalf("unexpected error %v", err)
-// 	}
-// 	if got != 1 {
-// 		t.Errorf("want 1 got %d", got)
-// 	}
-// 	// we make sure that all expectations were met
-// 	if err := mock.ExpectationsWereMet(); err != nil {
-// 		t.Errorf("there were unfulfilled expectations: %s", err)
-// 	}
-// }
+	query := `
+  UPDATE players
+  SET .*
+  WHERE id = \$1
+  RETURNING \*`
+
+	p := store.Player{
+		PlayerID:  182919996442279937,
+		RosterID:  382574876546039808,
+		FirstName: "Dominic",
+		LastName:  "Luklowski",
+		Alias:     "DataSlayer9",
+		Status:    "active",
+	}
+
+	mock.ExpectQuery(query).WithArgs(
+		p.PlayerID,
+		p.RosterID,
+		p.FirstName,
+		p.LastName,
+		p.Alias,
+		p.Status,
+	).WillReturnRows(rows)
+
+	ps := New(database.New(db, "mock-db", 0))
+	got, err := ps.Update(context.Background(), p)
+	if err != nil {
+		t.Fatalf("unexpected error %v", err)
+	}
+	want := p
+	want.RosterID = 1
+	want.Status = "benched"
+	if !reflect.DeepEqual(got, &want) {
+		t.Errorf("want\n%+v\ngot\n%+v\n", got, want)
+	}
+	// we make sure that all expectations were met
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
